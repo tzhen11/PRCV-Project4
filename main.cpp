997dfed95260e75ae449b151f9a1776f95e7ee6e
Terry Zhen <44910256+tzhen11@users.noreply.github.com>
2026-03-17 18:10:34 -0400

    Task 3

--- main.cpp
+++ main.cpp
@@ -43,6 +43,9 @@ int main(int argc, char* argv[]) {
         Mat view = s.nextImage();
         if (view.empty()) break;
 
+        // Retrieve image size
+        Size image_size = view.size();
+
         bool found = findChessboardCorners(view, s.boardSize, corner_set,
             CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK);
 
@@ -73,6 +76,47 @@ int main(int argc, char* argv[]) {
             corner_list.push_back(last_corners);
             point_list.push_back(point_set);
             cout << "Saved frame " << corner_list.size() << endl;
+        }
+
+        if (key == 'c') {
+            if (corner_list.size() >= 5) {
+                // Initialize camera matrix
+                Mat camera_matrix = Mat::eye(3, 3, CV_64FC1);
+                camera_matrix.at<double>(0, 2) = image_size.width / 2;
+                camera_matrix.at<double>(1, 2) = image_size.height / 2;
+
+                Mat distortion_coefficients = Mat::zeros(5, 1, CV_64FC1);
+                vector<Mat> rotation_vecs, translation_vecs;
+
+                cout << "Camera matrix before calibration:" << endl << camera_matrix << endl;
+                cout << "Distortion coefficients before calibration:" << endl << distortion_coefficients << endl;
+
+                // Run calibration
+                double reprojection_error = calibrateCamera(
+                    point_list,
+                    corner_list,
+                    image_size,
+                    camera_matrix,
+                    distortion_coefficients,
+                    rotation_vecs,
+                    translation_vecs,
+                    CALIB_FIX_ASPECT_RATIO
+                );
+
+                cout << "Camera matrix after calibration:" << endl << camera_matrix << endl;
+                cout << "Distortion coefficients after calibration:" << endl << distortion_coefficients << endl;
+                cout << "Reprojection error: " << reprojection_error << endl;
+
+                // save intrinsic parameters to file
+                FileStorage out("intrinsics.xml", FileStorage::WRITE);
+                out << "camera_matrix" << camera_matrix;
+                out << "distortion_coefficients" << distortion_coefficients;
+                out.release();
+                cout << "Intrinsics saved to intrinsics.xml" << endl;
+            }
+            else {
+                cout << "Not enough samples saved for calibration" << endl;
+            }
         }
     }
 
